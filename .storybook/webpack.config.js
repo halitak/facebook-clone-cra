df6f7bd977b915b8b97ea375b3341ae3b4fe1124
Halit Ak <ak.halit@outlook.com>
2020-08-16 23:20:17 +0300

    storybook ayarları yapıldı

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = ({ config, mode }) => {
+    config.module.rules.push({
+        test: /\.(ts|tsx)$/,
+        use: [
+            {
+                loader: require.resolve('babel-loader'),
+                options: {
+                    presets: [require.resolve('babel-preset-react-app')],
+                },
+            },
+            require.resolve('react-docgen-typescript-loader'),
+        ],
+    });
+    config.module.rules.push({
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+        include: path.resolve(__dirname, '../'),
+    });
+    config.resolve.extensions.push('.ts', '.tsx');
+    return config;
+};
